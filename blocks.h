9e1548fb62227c2b150cf30e65b2506fe92267c6
CodingdAwn <gaoxifengfordawn@gmail.com>
2026-02-01 18:00:39 +0800

    update music status

--- blocks.h
+++ blocks.h
@@ -1,8 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	30,		0},
-	{"", "date '+%m/%d %a %I:%M%p'",					60,		0},
+	/*Icon*/	/*Command*/		                                              /*Update Interval*/	/*Update Signal*/
+	{"", "/home/dawn/dotfiles/scripts/bin/music_status.sh",	 0,  10},
+	{"", "/home/dawn/dotfiles/scripts/bin/bar_status.sh",	   30, 0},
+	{"", "date '+%m/%d %a %I:%M%p'",	                       60, 0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
