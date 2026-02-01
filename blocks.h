b602f217eff4e955a9639a9076b924e7094a7547
CodingdAwn <gaoxifengfordawn@gmail.com>
2026-02-01 11:55:37 +0800

    update first dwmblocks modify

---
+++ blocks.h
@@ -0,0 +1,10 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"Mem:", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	30,		0},
+	{"", "date '+%m/%d %a %I:%M%p'",					60,		0},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
